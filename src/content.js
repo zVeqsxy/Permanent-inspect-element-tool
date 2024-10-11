e71bf7f93bc8f2b6025f833104c19b12495db63b
zVeqsxy <Reyhamudi609@gmail.com>
2024-10-11 19:48:41 +0200

    Fix: Ensure persistent changes and bypass CSP issues

--- src/content.js
+++ src/content.js
@@ -3,15 +3,35 @@ const storedChangesString = localStorage.getItem(currentURLContent + "zVeqsxy-in
 
 if (storedChangesString) {
   const storedChanges = JSON.parse(storedChangesString);
-  
-  storedChanges.forEach((change) => {
-    const elementSelector = change.path;
-    const elementHTML = change.html;
 
-    const element = document.querySelector(elementSelector);
-    
-    if (element) {
-      element.outerHTML = elementHTML;
-    }
+  const applyChanges = () => {
+    observer.disconnect();
+
+    storedChanges.forEach((change) => {
+      const elementSelector = change.path;
+      const elementHTML = change.html;
+
+      const element = document.querySelector(elementSelector);
+
+      if (element) {
+        element.outerHTML = elementHTML;
+      }
+    });
+
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true,
+    });
+  };
+
+  const observer = new MutationObserver((mutations) => {
+    applyChanges();
+  });
+
+  applyChanges();
+
+  observer.observe(document.body, {
+    childList: true,
+    subtree: true,
   });
 }
